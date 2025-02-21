81f56fe3dc9db5ab45d18a18a086f3f69b693625
A.Lejeune <acelyalejeune@gmail.com>
2025-02-21 15:50:52 +0100

    Update quote.component.ts

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { QuoteService } from '../../services/quote.service';
 
 @Component({
   selector: 'app-quote',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.scss'],
 })
 export class QuoteComponent implements OnInit {
-  quote: string = '';
-  author: string = '';
+  quote: string = 'Stay positive and happy. Work hard and don’t give up hope.';
+  author: string = 'Anonymous';
 
   constructor(private quoteService: QuoteService) {}
 
   ngOnInit(): void {
-    this.quoteService.getDailyQuote().subscribe((data: any) => {
-      this.quote = data.content;
-      this.author = data.author;
+    this.quoteService.getDailyQuote().subscribe({
+      next: (data) => {
+        this.quote = data.content;
+        this.author = data.author;
+      },
+      error: () => {
+        console.error('Failed to fetch quote. Using fallback quote.');
+      },
     });
   }
 }
