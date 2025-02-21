0a7d264db5c1cc5c967505110168d7d43eddb29d
A.Lejeune <acelyalejeune@gmail.com>
2025-02-21 15:00:59 +0100

    Update quote.service.ts

--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuoteService {
+  private apiUrl = 'https://api.quotable.io/random';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDailyQuote(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
 }
