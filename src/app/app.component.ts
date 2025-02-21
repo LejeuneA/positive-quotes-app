2840c7e4d2caaa0353461f2c13c7e964dcfccdd7
A.Lejeune <acelyalejeune@gmail.com>
2025-02-21 15:00:36 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router'; // Import RouterModule
+import { ThemeService } from './services/theme.service';
+import { SearchComponent } from './components/search/search.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Mark the component as standalone
+  imports: [RouterModule, SearchComponent, QuoteComponent, BackgroundComponent], // Add RouterModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'positive-quotes-app';
+
+  constructor(private themeService: ThemeService) {}
+
+  // Toggle between light and dark mode
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+
+  // Check if dark mode is enabled
+  get isDarkMode(): boolean {
+    return this.themeService.isDarkMode();
+  }
 }
