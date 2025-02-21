cb900361fd1420d45d1dcfd03386b7785a07b6c1
A.Lejeune <acelyalejeune@gmail.com>
2025-02-21 15:50:48 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,4 @@
 import { Routes } from '@angular/router';
 import { QuoteComponent } from './components/quote/quote.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { QuoteHistoryComponent } from './components/quote-history/quote-history.component';
-import { SettingsComponent } from './components/settings/settings.component';
 
-export const routes: Routes = [
-  { path: '', component: QuoteComponent },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'history', component: QuoteHistoryComponent },
-  { path: 'settings', component: SettingsComponent },
-];
+export const routes: Routes = [{ path: '', component: QuoteComponent }];
