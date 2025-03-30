3c94e078e7245fcc48e514224c063381cedaa166
Acelya Lejeune <acelyalejeune@gmail.com>
2025-03-30 10:31:23 +0200

    created login and register components

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideRouter(routes)],
+  providers: [provideHttpClient(), provideRouter(routes), provideAnimationsAsync()],
 }).catch((err: any) => console.error(err));
