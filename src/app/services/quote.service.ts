4d38337096073d99c3116590bfbcc0d1f828e09e
A.Lejeune <acelyalejeune@gmail.com>
2025-06-09 15:09:02 +0200

    environments

--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuoteService {
-  private apiUrl = '/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
