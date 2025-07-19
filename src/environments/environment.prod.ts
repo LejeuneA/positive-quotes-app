2ccc63376edd40463762c657b79c9ca38de72369
A.Lejeune <acelyalejeune@gmail.com>
2025-07-19 17:46:18 +0200

    compile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.quotable.io',
-  jsonServerUrl: 'https://positive-quotes-server.acelyalejeune.repl.com',
+  jsonServerUrl: 'https://positive-quotes-server.acelyalejeune.repl.co',
 };
