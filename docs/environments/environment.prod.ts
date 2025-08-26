294f9635c2e4b869c93e7e60b96a2aa409774687
A.Lejeune <acelyalejeune@gmail.com>
2025-08-26 10:20:07 +0200

    replit

--- docs/environments/environment.prod.ts
+++ docs/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.quotable.io',
-  jsonServerUrl:
-    'https://e9acb16d-1efd-4c4c-a807-317fb5fe118b-00-kbg78dfsei1n.kirk.replit.dev',
+  jsonServerUrl: 'https://workspace.acelyalejeune.repl.co',
 };
--- docs/environments/environment.ts
+++ docs/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: '/api',
-  jsonServerUrl: 'http://localhost:3000',
+  jsonServerUrl: 'http://localhost:3001',
 };
